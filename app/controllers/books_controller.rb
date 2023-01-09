d0f81ad6c64531b864bad73097f9e0ea1242b9cd
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-09 13:20:54 +0000

    [Update] チェックシート：バリテーションの実装(css未実装)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
+
+  Suc = "Book was successfully created."
+
   def top
+  end
+
+  def new
   end
 
   def index
@@ -8,9 +14,15 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = Suc
+      redirect_to book_path(@book.id)
+    else
+      # redirect_to '/books'
+      render:index
+    end
   end
 
   def show
@@ -22,9 +34,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = Suc
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/top' => 'books#top'
   get 'books' => 'books#index'
+  get 'books' => 'books#new'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
